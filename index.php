cdf0f4b414c7476314fadc831dd78897ad7bfe6d
dokyasde <dokyasde@gmail.com>
2018-07-13 15:25:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-$GithubRawURI= "https://raw.githubusercontent.com/dokyasde/dokyasde.github.io/master/index.php”;
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $GithubRawURI);
-curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$data = curl_exec($ch);
-curl_close($ch);
- 
-//remove the "<?php" from the file before running it
-$data = substr($data, 5);
- 
-eval ($data);
+<h1>Hello Word!</h1>
+
+github.io hosting free
